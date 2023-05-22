5dde6250f94c09adb9f35653c0e256dcad5e3f05
Benjamin Axline <baxline@bu.edu>
2023-05-22 16:14:05 +1000

    this

--- FA.v
+++
@@ -1,9 +0,0 @@
-module FA(a,b,cin,cout,s);
-	//all inputs and outputs are 1-bit
-	input a, b, cin;
-	output cout, s;
-
-	assign cout = (a&b&(~cin)) | ((~a)&b&cin) | (a&(~b)&cin) | (a&b&cin);
-	assign s = ((~a)&b&(~cin)) | (a&(~b)&(~cin)) | ((~a)&(~b)&cin) | (a&b&cin);
-
-endmodule
--- mov.v
+++
@@ -1,6 +0,0 @@
-module mov(Rx, Ry);
-    input [3:0] Rx, Ry;
-
-    
-
-endmodule   
