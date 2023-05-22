521e2b02790c93187e73c8bbb416a42943b701ec
Benjamin Axline <baxline@bu.edu>
2023-05-22 17:21:42 +1000

    8 reg

--- load_TB.v
+++ load_TB.v
@@ -9,7 +9,7 @@ module mod_TB;
 	reg [3:0] func;
 	wire data_out, a_in, g_out, g_in,  AddSub, AS_enable, XOR_enable;
 	reg [4:0] count;
-	wire [3:0] reg_in, reg_out;
+	wire [7:0] reg_in, reg_out;
 	wire [4:0] current_state;
 	wire [6:0] math_out;
 
@@ -20,15 +20,24 @@ module mod_TB;
 	//registers
 	tri_buf D(.a(in2), .enable(data_out), .b(bus));
 
-	register R0(.x(bus), .clk(clk), .enable(reg_in[3]), .y(t0));
-	register R1(.x(bus), .clk(clk), .enable(reg_in[2]), .y(t1));
-	register R2(.x(bus), .clk(clk), .enable(reg_in[1]), .y(t2));
-	register R3(.x(bus), .clk(clk), .enable(reg_in[0]), .y(t3));
+	register R0(.x(bus), .clk(clk), .enable(reg_in[7]), .y(t0));
+	register R1(.x(bus), .clk(clk), .enable(reg_in[6]), .y(t1));
+	register R2(.x(bus), .clk(clk), .enable(reg_in[5]), .y(t2));
+	register R3(.x(bus), .clk(clk), .enable(reg_in[4]), .y(t3));
+	register R4(.x(bus), .clk(clk), .enable(reg_in[3]), .y(t4));
+	register R5(.x(bus), .clk(clk), .enable(reg_in[2]), .y(t5));
+	register R6(.x(bus), .clk(clk), .enable(reg_in[1]), .y(t6));
+	register R7(.x(bus), .clk(clk), .enable(reg_in[0]), .y(t7));
 
-	tri_buf T0(.a(t0), .enable(reg_out[3]), .b(bus));
-	tri_buf T1(.a(t1), .enable(reg_out[2]), .b(bus));
-	tri_buf T2(.a(t2), .enable(reg_out[1]), .b(bus));
-	tri_buf T3(.a(t3), .enable(reg_out[0]), .b(bus));
+
+	tri_buf T0(.a(t0), .enable(reg_out[7]), .b(bus));
+	tri_buf T1(.a(t1), .enable(reg_out[6]), .b(bus));
+	tri_buf T2(.a(t2), .enable(reg_out[5]), .b(bus));
+	tri_buf T3(.a(t3), .enable(reg_out[4]), .b(bus));
+	tri_buf T4(.a(t4), .enable(reg_out[3]), .b(bus));
+	tri_buf T5(.a(t5), .enable(reg_out[2]), .b(bus));
+	tri_buf T6(.a(t6), .enable(reg_out[1]), .b(bus));
+	tri_buf T7(.a(t7), .enable(reg_out[0]), .b(bus));
 
 	register A(.x(bus), .clk(clk), .enable(a_in), .y(a_out));
 
@@ -66,23 +75,23 @@ module mod_TB;
 	
 	always @(count) begin
 		case (count)
-			5'b00000 : begin clk = 1; func = 4'b0001; in1 = 3'b000; in2 = 3'b011; end
+			5'b00000 : begin clk = 1; func = 4'b0001; in1 = 3'b000; in2 = 3'b101; end
 			5'b00001 : begin clk = 0; end
-			5'b00010 : begin clk = 1; func = 4'b0001; in1 = 3'b001; in2 = 3'b101; end
+			5'b00010 : begin clk = 1; func = 4'b0001; in1 = 3'b100; in2 = 3'b101; end
 			5'b00011 : begin clk = 0; end
 			5'b00100 : begin clk = 1; func = 4'b0001; in1 = 3'b010; in2 = 3'b100; end
 			5'b00101 : begin clk = 0; end
-			5'b00110 : begin clk = 1; func = 4'b0001; in1 = 3'b011; in2 = 3'b001; end
+			5'b00110 : begin clk = 1; func = 4'b0001; in1 = 3'b101; in2 = 3'b001; end
 			5'b00111 : begin clk = 0; end
-			5'b01000 : begin clk = 1; func = 4'b0010; in1 = 3'b001; in2 = 3'b011; end
+			5'b01000 : begin clk = 1; func = 4'b0010; in1 = 3'b000; in2 = 3'b101; end
 			5'b01001 : begin clk = 0; end
 			5'b01010 : begin clk = 1; func = 4'b0010; in1 = 3'b010; in2 = 3'b000; end
 			5'b01011 : begin clk = 0; end
-			5'b01100 : begin clk = 1; func = 4'b1001; in1 = 3'b000; in2 = 3'b001; end
+			5'b01100 : begin clk = 1; func = 4'b1001; in1 = 3'b000; in2 = 3'b101; end
 			5'b01101 : begin clk = 0; end
-			5'b01110 : begin clk = 1; func = 4'b1001; in1 = 3'b000; in2 = 3'b001; end
+			5'b01110 : begin clk = 1; func = 4'b1001; in1 = 3'b000; in2 = 3'b101; end
 			5'b01111 : begin clk = 0; end
-			5'b10000 : begin clk = 1; func = 4'b1001; in1 = 3'b000; in2 = 3'b001; end
+			5'b10000 : begin clk = 1; func = 4'b1001; in1 = 3'b000; in2 = 3'b101; end
 			5'b10001 : begin clk = 0; end
 
 
--- reg_decoder.v
+++ reg_decoder.v
@@ -1,19 +1,19 @@
 module reg_decoder(reg_num, clk, reg_enable);
     input clk;
     input [2:0] reg_num;
-    output reg [3:0] reg_enable; //8 registers
+    output reg [7:0] reg_enable; //8 registers
 
     always @(clk) begin
         case(reg_num)
-            3'b000 : reg_enable = 4'b1000; //0000; 
-            3'b001 : reg_enable = 4'b0100; //0000;
-            3'b010 : reg_enable = 4'b0010; //0000;
-            3'b011 : reg_enable = 4'b0001; //0000;
-            // 3'b100 : reg_enable = 8'b00001000; 
-            // 3'b101 : reg_enable = 8'b00000100;
-            // 3'b110 : reg_enable = 8'b00000010;
-            // 3'b111 : reg_enable = 8'b00000001;
-            default : reg_enable = 4'b0000; //0000; end
+            3'b000 : reg_enable = 8'b10000000; 
+            3'b001 : reg_enable = 8'b01000000;
+            3'b010 : reg_enable = 8'b00100000;
+            3'b011 : reg_enable = 8'b00010000;
+            3'b100 : reg_enable = 8'b00001000; 
+            3'b101 : reg_enable = 8'b00000100;
+            3'b110 : reg_enable = 8'b00000010;
+            3'b111 : reg_enable = 8'b00000001;
+            default : reg_enable = 8'b00000000; 
         endcase
     end
 endmodule
